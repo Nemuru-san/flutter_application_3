e1a6ea16886b85372130ad20f683e2a888522dd9
Nemuru-san <nimemanta@gmail.com>
2021-09-19 13:32:57 +0700

    First commit Login Form

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const primaryColor = Color(0xffff4081);
+  static const primaryDarkColor = Color(0xff009688);
+  static const underlineTextField = Color(0xffffc400);
+  static const hintColor = Color(0xff212121);
+}
---
+++ lib/screens/login_view.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/constants.dart';
+import 'package:flutter_application_3/screens/register_view.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorPalette.primaryColor,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconLogin(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconLogin() {
+    return Image.asset(
+      "assets/images/anime.png",
+      width: 150.0,
+      height: 150.0,
+    );
+  }
+
+  Widget _titleDescription() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 18),
+        ),
+        Text(
+          "Login into app",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt ante lacus, eu pretium purus vulputate sit amet. by Mhd. Arfin Alfaridz R",
+          style: TextStyle(
+              fontSize: 14.0, color: Colors.black, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 14.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+            icon: Icon(Icons.people),
+          ),
+          style: TextStyle(color: Colors.white),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+            icon: Icon(Icons.security),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          autofocus: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 20.0),
+        ),
+        InkWell(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: Text(
+              'Login Now',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          'or',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14.0,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Register',
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, RegisterPage.routeName);
+          },
+        ),
+      ],
+    );
+  }
+}
